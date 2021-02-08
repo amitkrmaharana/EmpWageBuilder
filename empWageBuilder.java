303cc9607cad347d39060214d81ca4d50073de15
amitkrmaharana <ak657harley@gmail.com>
2021-02-08 15:09:19 +0530

    Refactored: Added empWageBuilder.java

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,11 +1,16 @@
 public class empWageBuilder {
 	public static void main(String[] args) {
-		 System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		int IS_FULL_TIME = 1;
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int dailyWage = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
+		if (empCheck == IS_FULL_TIME) {
+			dailyWage = (wagePerHour * fullDayHour);
+			System.out.println("Daily Wage of the Employee is: " +dailyWage);
+		}
 		else
-			 System.out.println("Employee is Absent");
+			System.out.println("Daily Wage of the Employee is: " +dailyWage);
 	}
 }
