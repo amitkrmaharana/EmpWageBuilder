944b0288d5379521133f1b286a7148ea91a5059c
amitkrmaharana <ak657harley@gmail.com>
2021-02-08 15:16:42 +0530

    Refactored: Added empWageBuilder.java

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -2,13 +2,19 @@ public class empWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int wagePerHour = 20;
 		int fullDayHour = 8;
+		int partTimeHour = 4;
 		int dailyWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_FULL_TIME) {
 			dailyWage = (wagePerHour * fullDayHour);
 			System.out.println("Daily Wage of the Employee is: " +dailyWage);
+		}
+		else if (empCheck == IS_PART_TIME) {
+			dailyWage = (wagePerHour * partTimeHour);
+                        System.out.println("Daily Wage of the Employee is: " +dailyWage);
 		}
 		else
 			System.out.println("Daily Wage of the Employee is: " +dailyWage);
