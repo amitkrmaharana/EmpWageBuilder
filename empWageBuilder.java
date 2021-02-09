b723f32d66e8a0fe691000e930dc0a069d2009ba
Amit Kumar Maharana <77839157+amitkrmaharana@users.noreply.github.com>
2021-02-09 22:57:24 +0530

    Added: Employee attendance check

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,6 +1,6 @@
-public class empWageBuilder {
+public class EmpWageBuilder {
 	public static void main(String[] args) {
-		 System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		int IS_FULL_TIME = 1;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
