e2594e250fa11862a7d08e53d708c4436f001901
amitkrmaharana <ak657harley@gmail.com>
2021-02-09 09:38:44 +0530

    Refactor: solved using switch case

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,20 +1,23 @@
 public class empWageBuilder {
+
+
+	public static final int IS_FULL_TIME = 1;
+        public static final int IS_PART_TIME = 2;
+	public static final int WAGE_PER_HOUR = 20;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int wagePerHour = 20;
 		int fullDayHour = 8;
 		int partTimeHour = 4;
 		int dailyWage = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 3;
 		switch ((int)empCheck) {
-			case 1:
-				dailyWage = (wagePerHour * fullDayHour);
+			case IS_FULL_TIME:
+				dailyWage = (WAGE_PER_HOUR * fullDayHour);
 				System.out.println("Daily Wage of the Employee is: " +dailyWage);
 				break;
-			case 2:
-				dailyWage = (wagePerHour * partTimeHour);
+			case IS_PART_TIME:
+				dailyWage = (WAGE_PER_HOUR * partTimeHour);
 	                        System.out.println("Daily Wage of the Employee is: " +dailyWage);
 				break;
 			default:
