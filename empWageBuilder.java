79b61978552bc61f462033106f91ca841e151d45
Amit Kumar Maharana <77839157+amitkrmaharana@users.noreply.github.com>
2021-02-10 16:50:53 +0530

    Added: added total working hours and days

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -4,11 +4,11 @@ public class EmpWageBuilder {
 	public static final int IS_FULL_TIME = 1;
         public static final int IS_PART_TIME = 2;
 	public static final int WAGE_PER_HOUR = 20;
+	public static final int FULL_DAY_HOUR = 8;
+	public static final int PART_TIME_HOUR = 4;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-		int fullDayHour = 8;
-		int partTimeHour = 4;
 		int dailyWage = 0;
 		int monthlyWage = 0;
 		int totalWorkingHours = 0;
@@ -17,19 +17,18 @@ public class EmpWageBuilder {
 			double empCheck = Math.floor(Math.random() * 10) % 3;
 			switch ((int)empCheck) {
 				case IS_FULL_TIME:
-					dailyWage = (WAGE_PER_HOUR * fullDayHour);
+					dailyWage = (WAGE_PER_HOUR * FULL_DAY_HOUR);
 					monthlyWage += dailyWage;
 					totalWorkingHours += fullDayHour;
 					break;
 				case IS_PART_TIME:
-					dailyWage = (WAGE_PER_HOUR * partTimeHour);
+					dailyWage = (WAGE_PER_HOUR * PART_TIME_HOUR);
 		                        monthlyWage += dailyWage;
 					totalWorkingHours += partTimeHour;
 					break;
 				default:
 					dailyWage = 0;
 					monthlyWage += dailyWage;
-					break;
 			}
 			totalWorkingDays++;
 		}
