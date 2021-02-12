7cb3510de75fb9a9b8ad0b0aecef43d9f798c08b
amitkrmaharana <ak657harley@gmail.com>
2021-02-12 12:10:15 +0530

    Refactored: refactored to save totalwage for multiple companies

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,4 +1,3 @@
-package com.Br;
 public class EmpWageBuilder {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
@@ -15,7 +14,7 @@ public class EmpWageBuilder {
     	this.maxHoursPerMonth = maxHoursPerMonth;
     }
 
-    private int[] computeEmpWage() {
+    private int computeEmpWage() {
         int dailyWage = 0, monthlyWage = 0, totalWorkingHours = 0, totalWorkingDays = 0, empHr = 0;
         while (totalWorkingHours < maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
             double empCheck = Math.floor(Math.random() * 10) % 3;
@@ -34,22 +33,17 @@ public class EmpWageBuilder {
             monthlyWage += dailyWage;
             totalWorkingDays++;
         }
-        int[] wagelist = {totalWorkingHours, totalWorkingDays, monthlyWage};
-        return wagelist;
+        return monthlyWage;
     }
 
     public static void main(String[] args) {
-            System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-            EmpWageBuilder dMart = new EmpWageBuilder("DMart", 20, 13, 50);
-            int[] array = dMart.computeEmpWage();
-            EmpWageBuilder reliance = new EmpWageBuilder("RELIANCE", 20, 13, 50);
-            int[] array2 = reliance.computeEmpWage();
-            System.out.println("The total working hours of the employee of company " + dMart.company + " is " + array[0]);
-            System.out.println("The total working days of the employee of company " + dMart.company + " is " + array[1]);
-            System.out.println("The monthly wage of the employee of company " + dMart.company + " is " + array[2]);
-            System.out.println("The total working hours of the employee of company " + reliance.company + " is " + array2[0]);
-            System.out.println("The total working days of the employee of company " + reliance.company + " is " + array2[1]);
-            System.out.println("The monthly wage of the employee of company " + reliance.company + " is " + array2[2]);
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        EmpWageBuilder dMart = new EmpWageBuilder("DMart", 20, 13, 50);
+	EmpWageBuilder reliance = new EmpWageBuilder("RELIANCE", 23, 15, 70);
+	EmpWageBuilder tataMotors = new EmpWageBuilder("TATA MOTORS", 18, 19, 130);
+	System.out.println("The total monthly Wage of the Employee of the company " + dMart.company + " is " + dMart.computeEmpWage());
+	System.out.println("The total monthly Wage of the Employee of the company " + reliance.company + " is " + reliance.computeEmpWage());
+	System.out.println("The total monthly Wage of the Employee of the company " + tataMotors.company + " is " + tataMotors.computeEmpWage());
     }
 
 }
